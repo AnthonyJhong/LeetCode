4f9bab702fc21c4634d15471e98d711e64a5fde0
AnthonyJhong <anthonyjhong@gmail.com>
2022-09-13 22:28:20 -0700

    Caintains Duplicates 3

---
+++ java/com/leetcode/practice/ContainsDuplicate3.java
@@ -0,0 +1,28 @@
+package com.leetcode.practice;
+
+import java.lang.Math;
+
+public class ContainsDuplicate3 {
+    public static void main(String [] args){
+        //System.out.println(containsNearbyAlmostDuplicate(new int [] {}, 0, 0));
+        System.out.println(containsNearbyAlmostDuplicate(new int [] {1,2,3,4}, 3, 3));
+        System.out.println(containsNearbyAlmostDuplicate(new int [] {1,5,9,1,5,9}, 2, 3));
+    }
+    public static boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff){
+        int iterK = indexDiff;
+        for(int i = 0; i < nums.length; i++){
+
+            if(i + indexDiff > nums.length-1)
+                iterK = nums.length-1 - i;
+            else
+                iterK = indexDiff;
+
+            for(int j = 1; j <= iterK; j++){
+                if(Math.abs(nums[i] - nums[i+j]) <= valueDiff)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+}
